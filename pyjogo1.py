200e4bab150d05486b1e51877e21a12c1543ce61
Nelson Jr <juniordell123@gmail.com>
2020-05-08 14:58:28 -0300

    Add sound and new rectangles

--- pyjogo1.py
+++ pyjogo1.py
@@ -19,13 +19,22 @@ def main():
     ret = pygame.Rect(10, 10, 30, 30)
     ret2 = pygame.Rect(0, 50, 660, 20)
     ret3 = pygame.Rect(580, 450, 30, 30)
-
+    ret4 = pygame.Rect(200, 110, 500, 20)
+    ret5 = pygame.Rect(0, 110, 150, 20)
+    ret6 = pygame.Rect(20, 180, 165, 20)
+    square = pygame.Rect(20, 140, 30, 30)
+    bar1 = pygame.Rect(0, 50, 20, 560)
+    bar2 = pygame.Rect(680, 110, 20, 450)
+    bars = [bar1, bar2]
     sair = False
     alet = (randint(0, 255), randint(0, 255), randint(0, 255))
     alet2 = (randint(0, 255), randint(0, 255), randint(0, 255))
     font = pygame.font.get_default_font()
     scoretext = pygame.font.SysFont(font, 30)
     pygame.font.init()
+    acerto = pygame.mixer.Sound('acerto.ogg')
+    explosao = pygame.mixer.Sound('explosão.ogg')
+    velocidade = 1
 
     while not sair:
 
@@ -43,6 +52,9 @@ def main():
         time = relogio.tick(60)
         screen.fill(white)
         screen.blit(sup, [0, 0])
+        square.move_ip(velocidade * time, 0)
+        if square.collidelist(bars) >= 0:
+            velocidade = -velocidade
 
         (xant, yant) = (ret.left, ret.top)
         (ret.left, ret.top) = pygame.mouse.get_pos()
@@ -51,12 +63,15 @@ def main():
 
         if ret.colliderect(ret2):
             score = 0
+            explosao.play()
             pygame.mouse.set_pos(15, 23)
+            explosao.set_volume(1)
             (ret.left, ret.top) = (xant, yant)
 
         if ret.colliderect(ret3):
             score += 1
             pygame.mouse.set_pos(15, 23)
+            acerto.play()
             (ret.left, ret.top) = (xant, yant)
 
         text2 = scoretext.render(f'Score: {score}', 1, green)
@@ -64,7 +79,12 @@ def main():
         pygame.draw.rect(screen, alet, ret)
         pygame.draw.rect(screen, alet2, ret2)
         pygame.draw.rect(screen, green, ret3)
-
+        pygame.draw.rect(screen, alet2, ret4)
+        pygame.draw.rect(screen, alet2, ret5)
+        pygame.draw.rect(screen, alet2, square)
+        pygame.draw.rect(screen, alet2, bar1)
+        pygame.draw.rect(screen, alet2, bar2)
+        pygame.draw.rect(screen, alet2, ret6)
         screen.blit(text2, (550, 500))
         pygame.display.update()
 
